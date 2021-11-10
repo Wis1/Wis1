03ca515109c38865cd6521d80c149640446a8901
Szymon Wizdorf <szymekwizdorf@gmail.com>
2021-11-10 10:32:45 +0100

    Added Testing.main

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,33 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main (String[]args){
-        System.out.println("Moduł 6.Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser= new SimpleUser("theForumUser");
+
+        String result= simpleUser.getUserName();
+
+        if (result.equals("theForumUser"))
+            System.out.println("Test ok");
+        else
+            System.out.println("Error!");
+
+        Calculator calculator= new Calculator();
+
+        int resultOfAdd= calculator.add(123,24);
+
+        if (resultOfAdd==147)
+            System.out.println("Test calculator.add ok");
+        else
+            System.out.println("calculator.add Error!");
+
+        int resultOfSubtract= calculator.subtract(123,24);
+
+        if (resultOfSubtract==99)
+            System.out.println("Test calculator.subtract ok");
+        else
+            System.out.println("calculator.subtract error!");
     }
 }
