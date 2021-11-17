d288e37d037dca6a789522a56ea9c0dc03151edd
Szymon Wizdorf <szymekwizdorf@gmail.com>
2021-11-17 20:28:26 +0100

    7.3 Added StreamMain.java

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,33 +1,29 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.beautifier.PoemDecorator;
-import com.kodilla.stream.lambda.*;
-import com.kodilla.stream.reference.FunctionalCalculator;
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
+import com.kodilla.stream.person.People;
 
+import java.sql.SQLOutput;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
-        ExpressionExecutor expressionExecutor= new ExpressionExecutor();
 
-        System.out.println("Calculating expression with lambdas.");
-        expressionExecutor.executeExpression(10,5, (a,b)->a+b);
-        expressionExecutor.executeExpression(10,5,(a, b)->a-b);
-        expressionExecutor.executeExpression(10,5, (a, b)->a*b);
-        expressionExecutor.executeExpression(10,5, (a, b)->a/b);
+        Forum theForum= new Forum();
+        Map<Integer, ForumUser> theResultMapOfForumUsers= theForum.getUserList().stream()
+                .filter(forumUser->forumUser.getSex()=='M')
+                .filter(forumUser -> forumUser.getDateOfBirth().getDayOfYear()<2002)
+                .filter(forumUser -> forumUser.getNumberOfPostsPublished()>0)
+                .collect(Collectors.toMap(ForumUser::getIdentifier, forumUser -> forumUser));
 
-        System.out.println("Calculating expression with method references");
-        expressionExecutor.executeExpression(3,4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3,4, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3,4, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3,4, FunctionalCalculator::divideAByb);
-
-        PoemBeautifier poemBeautifier= new PoemBeautifier();
-        poemBeautifier.beautify("Programming",text -> text+":):)");
-        poemBeautifier.beautify("Java", text -> "<)(> "+text+" <)(>");
-        poemBeautifier.beautify("developer", text -> "**** "+ text+" ****");
-        poemBeautifier.beautify("junior", text -> "::::{} "+text+" {}::::");
-        poemBeautifier.beautify("example", text -> text.toUpperCase());
+        theResultMapOfForumUsers.entrySet().stream()
+                .map(entry->entry.getKey()+": "+ entry.getValue())
+                .forEach(System.out::println);
     }
 }
