79b8b7513cfd60b36c1c85cd872998fbbe048ea5
Szymon Wizdorf <szymekwizdorf@gmail.com>
2021-11-19 17:35:08 +0100

    7.5 Average

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,30 @@
+package com.kodilla.stream.array;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static com.kodilla.stream.array.ArrayOperations.getAverage;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ArrayOperationsTestSuite {
+
+    @Test
+    void testGetAverage (){
+        //Given
+        int[] numbers = new int[7];
+        numbers[0]=1;
+        numbers[1]=2;
+        numbers[2]=3;
+        numbers[3]=4;
+        numbers[4]=5;
+        numbers[5]=6;
+        numbers[6]=7;
+
+
+        //When
+        double averageResult=ArrayOperations.getAverage(numbers);
+        //Then
+        assertEquals(4.0, averageResult);
+    }
+}
