7a66afb9bc91bccbe95ec5984cfb84cc0f0403f6
Szymon Wizdorf <szymekwizdorf@gmail.com>
2021-11-26 15:48:57 +0100

    8.2 try/catch in main method

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -2,23 +2,24 @@ package com.kodilla.exception.test;
 
 public class FirstChallenge {
     public double divide(double a, double b) throws ArithmeticException {
-        double divideResult=a/b;
-        try {  if (b == 0) {
-            throw new ArithmeticException();
-        }   return divideResult;
+         if (b == 0) {
+             throw new ArithmeticException();
+         }
+        return a/b;
+    }
+
+    public static void main(String[] args) {
+        FirstChallenge firstChallenge = new FirstChallenge();
+        double result=0;
+
+        try {
+            result = firstChallenge.divide(3, 0);
         } catch (ArithmeticException e) {
             System.out.println("You can't divide for zero. Error: " + e);
         } finally {
             System.out.println("An attempt was made to divide");
         }
-        return divideResult;
-    }
-
-    public static void main(String[] args) {
-        FirstChallenge firstChallenge = new FirstChallenge();
-        double result = firstChallenge.divide(3, 0);
 
         System.out.println(result);
     }
-
 }
