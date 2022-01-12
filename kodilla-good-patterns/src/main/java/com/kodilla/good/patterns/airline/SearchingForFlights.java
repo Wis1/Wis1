3d51626463a5aadc8b9087a26c2aebba717118d4
Szymon Wizdorf <szymekwizdorf@gmail.com>
2022-01-12 14:58:04 +0100

    14.1 ForumUser test

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/airline/SearchingForFlights.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/airline/SearchingForFlights.java
@@ -1,12 +1,10 @@
 package com.kodilla.good.patterns.airline;
 
-import java.time.LocalTime;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class SearchingForFlights {
     private ListOfAllFlights listOfAllFlights = new ListOfAllFlights();
-    private String city;
 
     public void searchingFromCity(String city) {
         listOfAllFlights.allOfFlights().entrySet().stream()
@@ -29,14 +27,14 @@ public class SearchingForFlights {
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
         Map<Integer, Flight> mapOfFlightsToDestinationCity = listOfAllFlights.allOfFlights().entrySet().stream()
                 .filter(entry -> entry.getValue().getCityOfArrival().equals(destinationCity))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+                .collect(Collectors.toMap(entry->entry.getKey(), entry->entry.getValue()));
 
         for (Map.Entry<Integer, Flight> flight1 : mapOfFlightsFromMyCity.entrySet()) {
             for (Map.Entry<Integer, Flight> flight2 : mapOfFlightsToDestinationCity.entrySet()) {
                 if (flight1.getValue().getCityOfArrival().equals(flight2.getValue().getDepartureCity()) &&
                         flight2.getValue().getTimeOfDeparture().compareTo(flight1.getValue().getTimeOfArrival()) > 0) {
-                    System.out.println(flight1.toString());
-                    System.out.println(flight2.toString());
+                    System.out.println(flight1);
+                    System.out.println(flight2);
                 }
             }
         }
