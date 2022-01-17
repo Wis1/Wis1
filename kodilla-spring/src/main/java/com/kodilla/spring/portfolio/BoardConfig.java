4066da373207a27b7da3b5fc08a6035e0eb2e059
Szymon Wizdorf <szymekwizdorf@gmail.com>
2022-01-17 18:23:21 +0100

    14.3 next version

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,28 +1,32 @@
 package com.kodilla.spring.portfolio;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BoardConfig {
 
     @Bean(name="list1")
+    @Scope("prototype")
     public TaskList list1(){
         return new TaskList();
     }
 
     @Bean(name="list2")
+    @Scope("prototype")
     public TaskList list2(){
         return new TaskList();
     }
 
     @Bean(name="list3")
+    @Scope("prototype")
     public TaskList list3(){
         return new TaskList();
     }
-    @Autowired
-    public Board getBoard( @Qualifier("list1") TaskList list1, @Qualifier("list2") TaskList list2 ,@Qualifier("list3") TaskList list3){
+
+
+    public Board getBoard( @Qualifier("list1") TaskList list1, @Qualifier("list2") TaskList list2 ,@Qualifier("list3") TaskList list3) {
         return new Board(list1, list2, list3);
     }
 }
