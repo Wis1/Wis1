c4713b15055885076b4452f65e3ed21ea73a0924
Szymon Wizdorf <szymekwizdorf@gmail.com>
2022-03-26 11:55:33 +0100

    24.3 aspects

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
@@ -2,6 +2,7 @@ package com.kodilla.patterns2.facade.api;
 
 import com.kodilla.patterns2.facade.ShopService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import java.math.BigDecimal;
 
 @Service
+@EnableAspectJAutoProxy
 public class OrderFacade {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderFacade.class);
